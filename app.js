3589bf84f50b141fb0685ca101fdcca6b47ee2c6
JuBragada <juliabragada@gmail.com>
2024-09-07 21:04:29 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+function music() {
+    let audio = document.getElementById("myAudio");
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  }
+function pesquisar() {
+    // Obtém a seção onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+    
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+    
+    campoPesquisa = campoPesquisa.toLowerCase()
+    
+    if(campoPesquisa== "") {
+       section.innerHTML = "Você precisa digitar o nome de um livro para ser pesquisado"
+        return
+    }
+    
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+    let titulo = "";
+    let autor = "";
+    let genero = "";
+    let sinopse = "";
+
+    // Itera sobre cada livro na lista de livros
+    for (let livro of livros) {
+      
+        titulo = livro.titulo.toLowerCase();
+        autor = livro.autor.toLowerCase();
+        genero = livro.genero.toLowerCase();
+        sinopse = livro.sinopse.toLowerCase();
+
+        if(titulo.includes(campoPesquisa) || autor.includes(campoPesquisa) || genero.includes(campoPesquisa) || sinopse.includes(campoPesquisa)) {
+             // Cria um novo elemento
+        resultados += `
+            <div class ="item-resultado">
+                <h2> ${livro.titulo}</h2>
+                <p class = "autor">${livro.autor}</p>
+                <p class = "genero">${livro.genero}</p>
+                <p class = "sinopse">${livro.sinopse}</p>
+            </div>
+        `;
+
+        }  
+    }
+    
+    if (!resultados) {
+        resultados = "Não foi encontrado nenhum livro citado por Rory Gilmore pela sua busca"
+    }
+    
+    // Atribui a string com os resultados ao conteúdo HTML da seção
+    section.innerHTML = resultados;
+    }
+
+    
---
+++ dados.js
@@ -0,0 +1,59 @@
+let livros  = [
+{
+    titulo: "1984",
+    autor: "George Orwell",
+    genero: "Ficção científica, distopia, romance político",
+    sinopse: "Em um futuro distópico, o ano é 1984. A Oceânia, um dos três superestados que dominam o mundo, é governada pelo Partido, um regime totalitário que controla todos os aspectos da vida dos cidadãos. Sob a vigilância constante do Grande Irmão, líder onipresente do Partido, a liberdade individual é eliminada e a verdade é distorcida para servir aos interesses do Estado. Winston Smith, um funcionário do Partido, começa a questionar a ordem estabelecida e a buscar a verdade. Seu desejo de liberdade o leva a se envolver em um romance proibido e a descobrir a existência de um movimento de resistência. No entanto, a paranoia e a repressão do Partido o perseguem, levando-o a uma jornada perigosa e repleta de incertezas."
+
+},
+
+{
+    titulo: "31 Canções",
+    autor: "Nick Hornby",
+    genero: "Não-ficção, Memórias, Música",
+    sinopse: "Em '31 Canções', Nick Hornby, o aclamado autor de 'Alta Fidelidade', nos convida a uma jornada pessoal e musical. Através de uma seleção de 31 canções que marcaram sua vida, o autor tece uma narrativa envolvente que entrelaça música, memórias e reflexões sobre a vida. Hornby não apenas lista as músicas, mas as utiliza como pontos de partida para explorar momentos-chave de sua existência: primeiros amores, perdas, viagens, a paternidade e muito mais. Cada canção é acompanhada de histórias e anedotas que revelam a importância da música em sua vida e como ela moldou sua identidade."
+},
+
+{
+    titulo: "Abadia de Northanger",
+    autor: "Jane Austen",
+    genero: "Romance, Ficção gótica, Sátira, Bildungsroman",
+    sinopse: "Catherine Morland, uma jovem apaixonada por romances góticos, é convidada a passar um tempo na Abadia de Northanger. Fascinada pela atmosfera misteriosa do lugar, ela começa a imaginar que está vivendo em um romance gótico, repleto de segredos e perigos. No entanto, à medida que se aprofunda no mundo da Abadia, Catherine descobre que a realidade é muito mais prosaica do que ela imaginava. A Abadia de Northanger é uma sátira aos romances góticos populares da época, e narra a jornada de Catherine em busca do amor e da maturidade."
+},
+{
+    titulo: "A Arte da Ficção",
+    autor: "Henry James",
+    genero: "Literatura, Crítica literária",
+    sinopse: "Um dos mais importantes ensaios sobre a escrita de ficção, 'A Arte da Ficção' de Henry James explora os elementos essenciais da narrativa, como ponto de vista, personagem e trama. James oferece insights valiosos sobre o processo criativo e a construção de histórias memoráveis."
+},
+{
+    titulo: "A Arte da Guerra",
+    autor: "Sun Tzu",
+    genero: "Estratégia militar, Filosofia",
+    sinopse: "Um clássico da estratégia militar, 'A Arte da Guerra' de Sun Tzu oferece princípios atemporais sobre liderança, planejamento e tática, que podem ser aplicados a diversas áreas da vida."
+},
+{
+    titulo: "A Bíblia Sagrada",
+    autor: "Diversos autores",
+    genero: "Religioso, Sagrado",
+    sinopse: "A Bíblia Sagrada é uma coleção de livros sagrados para o cristianismo, judaísmo e outras religiões abraâmicas. Ela contém narrativas históricas, leis, poesias, profecias e ensinamentos morais."
+},
+{
+    titulo: "A Bolt from the Blue and Other Essays",
+    autor: "Mary McCarthy",
+    genero: "Ensaio, Crítica literária",
+    sinopse: "Uma coleção de ensaios perspicazes de Mary McCarthy, onde ela aborda temas diversos como literatura, política e cultura popular. Com sua escrita ácida e inteligente, McCarthy oferece análises incisivas e provoca reflexões sobre a sociedade."
+},
+{
+    titulo: "A Cabana do Pai Tomás",
+    autor: "Harriet Beecher Stowe",
+    genero: "Romance, Anti-escravidão",
+    sinopse: "Um dos romances mais influentes da história americana, 'A Cabana do Pai Tomás' explora as atrocidades da escravidão e contribuiu significativamente para o movimento abolicionista."
+},
+{
+    titulo: "A Caldeira do Diabo",
+    autor: "Grace Metalious",
+    genero: "Romance, Realismo sujo",
+    sinopse: "Um romance polêmico que retrata a vida em uma pequena cidade americana, explorando temas como adultério, violência e hipocrisia. 'A Caldeira do Diabo' causou grande impacto ao ser publicado, devido à sua linguagem franca e ao retrato cru da sociedade."
+}
+]
