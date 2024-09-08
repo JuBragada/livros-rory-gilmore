efa5ee45347a6b7591a97a8483e8e25026d65d28
JuBragada <juliabragada@gmail.com>
2024-09-07 21:39:08 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -55,5 +55,96 @@ let livros  = [
     autor: "Grace Metalious",
     genero: "Romance, Realismo sujo",
     sinopse: "Um romance polêmico que retrata a vida em uma pequena cidade americana, explorando temas como adultério, violência e hipocrisia. 'A Caldeira do Diabo' causou grande impacto ao ser publicado, devido à sua linguagem franca e ao retrato cru da sociedade."
+},
+{
+    titulo: "A Casa dos Espíritos",
+    autor: "Isabel Allende",
+    genero: "Realismo mágico, Romance histórico",
+    sinopse: "Uma saga familiar épica que se estende por várias gerações, A Casa dos Espíritos entrelaça realismo mágico e história para contar a história da família Trueba, no Chile. A trama explora temas como amor, morte, política e a busca por identidade, tudo ambientado em um cenário marcado por grandes transformações sociais."
+},
+{
+    titulo: "A Célebre Rã Saltadora do Condado de Cavaleras",
+    autor: "Mark Twain",
+    genero: "Conto humorístico",
+    sinopse: "Uma sátira hilária e espirituosa, A Célebre Rã Saltadora do Condado de Cavaleras é um conto clássico de Mark Twain. A história gira em torno de uma competição de salto de rãs e explora temas como a ganância, a trapaça e a natureza humana. A narrativa é repleta de diálogos espirituosos e personagens excêntricos."
+},
+{
+    titulo: "A Comédia dos Erros",
+    autor: "William Shakespeare",
+    genero: "Comédia, Erro",
+    sinopse: "Uma das comédias mais populares de Shakespeare, A Comédia dos Erros é uma trama repleta de enganos e confusões identitárias. A história gira em torno de dois pares de gêmeos idênticos que se encontram em uma cidade estrangeira, desencadeando uma série de mal-entendidos e situações hilárias."
+},
+{
+    titulo: "A Divina Comédia",
+    autor: "Dante Alighieri",
+    genero: "Poema épico, Literatura medieval",
+    sinopse: "Considerada uma das maiores obras da literatura universal, A Divina Comédia é um poema épico em três partes que narra a jornada de Dante pelos Inferno, Purgatório e Paraíso. A obra explora temas como o pecado, a redenção e a busca pela perfeição divina, oferecendo uma visão profunda da condição humana e da natureza do universo."
+},
+{
+    titulo: "A Escolha de Sofia",
+    autor: "William Styron",
+    genero: "Romance histórico, Ficção",
+    sinopse: "Uma profunda exploração dos traumas da Segunda Guerra Mundial, 'A Escolha de Sofia' narra a história de uma sobrevivente de um campo de concentração nazista que é forçada a tomar uma decisão impossível: escolher qual dos seus dois filhos será enviado para a morte. A trama aborda temas como culpa, sobrevivência e a busca por significado em um mundo marcado pela violência."
+},
+{
+    titulo: "A Feira das Vaidades",
+    autor: "William Makepeace Thackeray",
+    genero: "Romance social, Satira",
+    sinopse: "Uma sátira da sociedade britânica do século XIX, 'A Feira das Vaidades' acompanha a ambiciosa Becky Sharp em sua ascensão social. A obra explora temas como hipocrisia, materialismo e a busca por status, oferecendo uma visão crítica da alta sociedade da época."
+},
+{
+    titulo: "A Heartbreaking Work of Staggering Genius",
+    autor: "Dave Eggers",
+    genero: "Memórias, Não-ficção",
+    sinopse: "Uma narrativa autobiográfica e comovente, 'A Heartbreaking Work of Staggering Genius' conta a história de Dave Eggers enquanto ele assume a responsabilidade de cuidar de seus dois irmãos mais novos após a morte de seus pais. A obra explora temas como família, perda, crescimento e a busca por significado em meio à adversidade."
+},
+{
+    titulo: "A História do Declínio e Queda do Império Romano",
+    autor: "Edward Gibbon",
+    genero: "História, Filosofia",
+    sinopse: "Uma obra monumental que analisa as causas do declínio e queda do Império Romano, 'A História do Declínio e Queda do Império Romano' é considerada um clássico da historiografia. Gibbon oferece uma interpretação complexa e multifacetada dos fatores que contribuíram para o fim de um dos maiores impérios da história."
+},
+{
+    titulo: "A Ilíada",
+    autor: "Homero",
+    genero: "Poema épico, Mitologia grega",
+    sinopse: "Um dos poemas épicos mais antigos e influentes da literatura ocidental, 'A Ilíada' narra um episódio da Guerra de Troia, a raiva de Aquiles e suas consequências. A obra explora temas como honra, vingança, amor e a natureza da guerra, oferecendo uma visão profunda da cultura e da sociedade gregas."
+},
+{
+    titulo: "A Letra Escarlate",
+    autor: "Nathaniel Hawthorne",
+    genero: "Romance histórico, Gótico",
+    sinopse: "Um clássico da literatura americana, 'A Letra Escarlate' narra a história de Hester Prynne, uma jovem puritana condenada ao ostracismo por ter um filho fora do casamento. A trama explora temas como pecado, hipocrisia e a busca por redenção em uma sociedade puritana."
+},
+{
+    titulo: "A Maratona da Morte",
+    autor: "William Goldman",
+    genero: "Thriller, Mistério",
+    sinopse: "Um thriller psicológico intenso, 'A Maratona da Morte' acompanha um grupo de corredores que participam de uma corrida de resistência em condições extremas. A trama é repleta de suspense e reviravoltas, explorando os limites da mente humana e a natureza da competição."
+},
+{
+    titulo: "A Metamorfose",
+    autor: "Franz Kafka",
+    genero: "Ficção existencialista, Conto",
+    sinopse: "Um conto atemporal e perturbador, 'A Metamorfose' narra a história de Gregor Samsa, um homem que acorda transformado em um inseto gigante. A obra explora temas como alienação, solidão e   
+a condição humana, oferecendo uma visão sombria e absurda da existência."
+},
+{
+    titulo: "A Monetary History of the United States",
+    autor: "Milton Friedman",
+    genero: "Economia",
+    sinopse: "Uma obra seminal sobre a história monetária dos Estados Unidos, 'A Monetary History of the United States' apresenta uma análise detalhada do papel do dinheiro na economia americana. Friedman argumenta que a política monetária desempenha um papel crucial na determinação da inflação e do desemprego."
+},
+{
+    titulo: "A Month Of Sundays: Searching For The Spirit And My Sister",
+    autor: "Julie Mars",
+    genero: "Memórias, Espiritualidade",
+    sinopse: "Uma jornada pessoal e inspiradora, 'A Month Of Sundays' narra a busca de Julie Mars por significado espiritual após a morte de sua irmã. A autora explora temas como fé, perda e a conexão entre a vida e a morte, oferecendo uma reflexão profunda sobre a experiência humana."
+},
+{
+    titulo: "A Morte de Um Caixeiro-Viajante",
+    autor: "Arthur Miller",
+    genero: "Drama",
+    sinopse: "Um clássico do teatro americano, 'A Morte de Um Caixeiro-Viajante' retrata a crise existencial de Willy Loman, um vendedor fracassado que busca o reconhecimento e o sucesso que nunca alcançou. A peça explora temas como o sonho americano, a família e a inevitabilidade da morte."
 }
 ]
